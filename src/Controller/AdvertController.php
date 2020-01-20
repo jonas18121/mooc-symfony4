dad8880eddcd1ae2a816ce8bbb5c3311a61577ac
jonas <jonasxtrm971@gmail.com>
2020-01-20 15:35:43 +0100

    création de plusieurs templates, et ajout de fausses annonces

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -12,7 +12,31 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AdvertController extends AbstractController
 {
-    
+     //affiché les dernières annonces
+    public function menuAction($limit)
+    {
+        $listAdverts = [
+            ['id' => 2, 'title' => 'Recherche développeur Symfony'],
+            ['id' => 3, 'title' => 'mission développeur Angular/Symfony'],
+            ['id' => 4, 'title' => 'stage pour développeur php'],
+            ['id' => 5, 'title' => 'stage pour développeur python']
+        ];
+
+        return $this->render('advert/_menu.html.twig',[
+            'listAdverts' => $listAdverts,
+            'limit' => $limit
+        ]);
+    }
+
+    /**
+     * @Route("/annonces", name="OC_advert_annonces")
+     */
+    public function annonces()
+    {
+        return $this->render('advert/annonces.html.twig');
+    }
+
+
     /**
      * @Route("/{page}", name="OC_advert_index",
      *  requirements={
@@ -29,10 +53,42 @@ class AdvertController extends AbstractController
         {
             throw $this->createNotFoundException("page '{$page}' inexistante"); // on lance une erreur 404
         }
+
+        $listAdverts = [
+            [
+                'id' => 2, 
+                'title' => 'Recherche développeur Symfony',
+                'author' => 'Alexandre',
+                'content' => 'Nous recherchons un developpeur Symfony sur Nantes',
+                'date' => new \Datetime()
+            ],
+            [
+                'id' => 3, 
+                'title' => 'Mission développeur Angular/Symfony',
+                'author' => 'Kassandra',
+                'content' => 'Nous avons une misson pour un developpeur Angular/Symfony sur Nantes',
+                'date' => new \Datetime()
+            ],
+            [
+                'id' => 4, 
+                'title' => 'Stage pour développeur php',
+                'author' => 'Olvier',
+                'content' => 'Nous recherchons un developpeur pour un stage magique sur Nantes',
+                'date' => new \Datetime()
+            ],
+            [
+                'id' => 5, 
+                'title' => 'Stage pour développeur python',
+                'author' => 'Pierre',
+                'content' => 'Super stage pour un developpeur python sur Nantes',
+                'date' => new \Datetime()
+            ]
+        ];
         
 
         return $this->render('advert/index.html.twig', [
-            'name' => 'zoubert'
+            'name' => 'zoubert',
+            'listAdverts' => $listAdverts
         ]);
     }
 
@@ -46,11 +102,20 @@ class AdvertController extends AbstractController
     {
         $tag = $request->query->get('tag'); //pour une URL /advert/view/{id}?tag=une_valeur
         $ok = $request->query->get('ok'); 
+
+        $advert = [
+                'id' => 2, 
+                'title' => 'Recherche développeur Symfony',
+                'author' => 'Alexandre',
+                'content' => 'Nous recherchons un developpeur Symfony sur Nantes',
+                'date' => new \Datetime()
+            ];
         
         //return new Response("Affichage de l'annonce d'id : '{$id}' , avec le tag : {$tag} {$ok} ");
-        return $this->render('Advert/view.html.twig', [
+        return $this->render('advert/view.html.twig', [
             'id'  => $id, 
-            'tag' => $tag
+            'tag' => $tag,
+            'advert' => $advert
         ]);
 
         /*return $this->redirectToRoute("OC_advert_index");*/
@@ -67,6 +132,14 @@ class AdvertController extends AbstractController
      */
     public function add(Request $request)
     {
+        $advert = [
+            'id' => 2, 
+            'title' => 'Recherche développeur Symfony',
+            'author' => 'Alexandre',
+            'content' => 'Nous recherchons un developpeur Symfony sur Nantes',
+            'date' => new \Datetime()
+        ];
+
         if($request->isMethod('POST')) // si la requête est en POST
         {
             /* ici on traite le formulaire */ 
@@ -78,17 +151,26 @@ class AdvertController extends AbstractController
         }
 
         //sinon on affiche le formulaire
-        return $this->render('advert/add.html.twig');
+        return $this->render('advert/add.html.twig',[
+            'advert' => $advert
+        ]);
     }
 
     /**
-     * @Route("/advert/edit/{id}", requirements={
+     * @Route("/advert/edit/{id}", name="OC_advert_edit", requirements={
      *      "id" = "[0-9]+"
      * })
      */
     public function edit($id, Request $request)
     {
         /* ici récupération de $id */ 
+        $advert = [
+            'id' => 2, 
+            'title' => 'Recherche développeur Symfony',
+            'author' => 'Alexandre',
+            'content' => 'Nous recherchons un developpeur Symfony sur Nantes',
+            'date' => new \Datetime()
+        ];
 
         if($request->isMethod('POST')) // si la requête est en POST
         {
@@ -99,11 +181,13 @@ class AdvertController extends AbstractController
         }
 
         //sinon on affiche le formulaire
-        return $this->render('advert/edit.html.twig');
+        return $this->render('advert/edit.html.twig',[
+            'advert' => $advert
+        ]);
     }
 
     /**
-     * @Route("/advert/delete/{id}", requirements={
+     * @Route("/advert/delete/{id}", name="OC_advert_delete", requirements={
      *      "id" = "[0-9]+"
      * })
      */

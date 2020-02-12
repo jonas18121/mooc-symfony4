89da52b37fe70f2cc735135b39c9b1ee347c884f
jonas <jonasxtrm971@gmail.com>
2020-02-12 17:54:29 +0100

    construire un formulaire differemment selon les parametre dans AdvertType

--- src/Form/AdvertType.php
+++ src/Form/AdvertType.php
@@ -18,6 +18,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
 use App\Form\ImageType;
 use App\Form\CategoryType;
 
@@ -36,7 +39,7 @@ class AdvertType extends AbstractType
             ->add('author',     TextType::class)
             ->add('content',    TextareaType::class)
             ->add('date',       DateType::class)
-            ->add('published',  CheckboxType::class, [ 'required' => false ])
+            //->add('published',  CheckboxType::class, [ 'required' => false ])
             ->add('image',      ImageType::class) // le formulaire de la classe Image est imbriqué dans celui de Advert
 
             /* 
@@ -70,6 +73,41 @@ class AdvertType extends AbstractType
 
             ->add('save',       SubmitType::class)
         ;
+
+        
+        //CONSTRUIRE UN FORMULAIRE DIFFEREMMENT SELON DES PARAMETRE
+
+        //On ajoute une function qui va écouter un évènement
+        $builder->addEventListener(
+
+            /*1er argument: PRE_SET_DATA est l'évènement qui nous intérresse  */
+            FormEvents::PRE_SET_DATA, 
+            
+            /*2eme arguments: La function à executer lorsque l'évènnement est déclencher  */
+            function(FormEvent $event)
+            {
+
+                //on récupère l'objet sous-jacent
+                $advert = $event->getData();
+
+
+                if(null === $advert){
+                    return; // on sort de la fonction sans rien faire , si $advert est
+                }
+
+
+                if(!$advert->getPublished() || null === $advert->getId())
+                {
+                    //on ajoute un champ published
+                    $event->getForm()->add('published',  CheckboxType::class, [ 'required' => false ]);
+                }
+                else{
+                    //sinon on le supprime 
+                    $event->getForm()->remove('published');
+                }
+            }
+        );
+
     }
 
     /**
